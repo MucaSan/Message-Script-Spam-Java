c75b77e9b72b28fbccd966550be83b543f1117e9
svillar <villar.mucasan@gmail.com>
2024-01-08 01:15:17 -0300

    Uploading the first version!

---
+++ Main.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package br.com.treinaweb.main;
+import java.util.Scanner;
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/* this program will take a message from the user, and he'll be able to run a script
+		// this script will have the option to send a message only in an horizontal line, or perhaps,
+		// in a vertical line, breaking the line;
+		*/
+		System.out.println("This program gets a message from the user, and repeats it, in two types of configuration:")
+		System.out.println("1. The message will be sent breaking the line;");
+		System.out.println("2. The message will be sent not breaking the line;");
+		System.out.println("And after that, the user will be capable of running the message indefinitely or choose a given number of messages to be looped");
+		System.out.println("And such, he'd have the possibility to run the program again!");
+		
+		Scanner message_option = new Scanner(System.in);
+		Scanner message_repetitive_option = new Scanner(System.in);
+		String message_choice = message_option.nextLine();
+		String message_repetitive_choice = 
+		
+
+	}
+	public static void message_without_breaking_line(String msg) {
+		
+	}
+	public static void message_breaking_line(String msg) {
+		
+	}
+	
+
+}
